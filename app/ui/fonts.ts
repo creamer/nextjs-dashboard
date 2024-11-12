b064302e618ef463d1d8c142ad5cf6268f93ca74
오태겸 <taekyeom.oh@nhn.com>
2024-11-12 17:14:56 +0900

    fixed error

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Lusitana, Nanum_Gothic } from 'next/font/google';
 
 export const inter = Nanum_Gothic({ weight: "400" });
-export const lusitana = Lusitana({ weight: ["400", "700"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ['latin'] });
